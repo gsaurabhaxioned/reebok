47129842444cb72173047743d604a2e7b632ba6c
saurabh <saurabhg@axioned.com>
2022-03-24 12:07:37 +0530

    slick slider added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,46 @@ $(document).ready(function () {
         $(".banner-menus-container").slideToggle();
         $(this).toggleClass("cross");
     });
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  });
+          
 });
+
+
 
 let title = document.querySelectorAll(".banner-menus li span");
 title.forEach(i => {
